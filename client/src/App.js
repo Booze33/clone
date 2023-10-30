575009856786e0c311d76dbc7827a5d8e7c23989
Booze33 <tebetisloh@gmail.com>
2023-10-30 03:55:39 +0100

    Added theme

--- client/src/App.js
+++ client/src/App.js
@@ -57,32 +57,32 @@ function App() {
     }
   })
 
-//   return (
-//     <div className="dark:bg-dark dark:text-dark w-screen h-52" >
-//       <div className="fixed top-5 right-10 duration-100 dark:bg-slate-700 bg-gary-100 rounded-full">
-//         {
-//           options?.map(opt => (
-//             <button
-//               key={opt.text}
-//               className={`w-8 h-8 leading-9 text-xl rounded-full m-1 ${theme === opt.text && "text-sky-600"}`}
-//               onClick={()=> setTheme(opt.text)}>
-//               <ion-icon name={opt.icon}></ion-icon>
-//             </button>
-//           ))
-//         }
+  return (
+    <div className="dark:bg-dark dark:text-dark w-screen h-52" >
+      <div className="fixed top-5 right-10 duration-100 dark:bg-slate-700 bg-gary-100 rounded-full">
+        {
+          options?.map(opt => (
+            <button
+              key={opt.text}
+              className={`w-8 h-8 leading-9 text-xl rounded-full m-1 ${theme === opt.text && "text-sky-600"}`}
+              onClick={()=> setTheme(opt.text)}>
+              <ion-icon name={opt.icon}></ion-icon>
+            </button>
+          ))
+        }
         
-//       </div>
+      </div>
 
-//       <h1>Theme</h1>
-//       <p>lorem this is the test for handling theme pray it works</p>
-//       <p>lorem this is the test for handling theme pray it works</p>
-//       <p>lorem this is the test for handling theme pray it works</p>
-//       <p>lorem this is the test for handling theme pray it works</p>
+      <h1>Theme</h1>
+      <p>lorem this is the test for handling theme pray it works</p>
+      <p>lorem this is the test for handling theme pray it works</p>
+      <p>lorem this is the test for handling theme pray it works</p>
+      <p>lorem this is the test for handling theme pray it works</p>
 
-//       {/* <button onClick={() => handleThemeChange('dark')}>Set Dark Theme</button>
-//       <button onClick={() => handleThemeChange('light')}>Set Light Theme</button> */}
-//     </div>
-//   );
+      {/* <button onClick={() => handleThemeChange('dark')}>Set Dark Theme</button>
+      <button onClick={() => handleThemeChange('light')}>Set Light Theme</button> */}
+    </div>
+  );
 }
 
 export default App;
